29f3b98b2bd812971ad28ec20b37921ef396f5b8
BertZZZZ <bert@schoofs-ven.com>
2017-07-15 16:05:17 +0200

    Shortcut after in english active

--- wme-emptystreet.js
+++ wme-emptystreet.js
@@ -13,13 +13,16 @@
 // @author         Bert Schoofs '2017
 // @license        MIT/BSD/X11
 // @icon
-// @require https://greasyfork.org/scripts/24851-wazewrap/code/WazeWrap.js?version=203372
+// @require https://greasyfork.org/scripts/16071-wme-keyboard-shortcuts/code/WME%20Keyboard%20Shortcuts.js
 // ==/UserScript==
 /* Changelog
 
 */
 
-var VERSION = '0.0.1';
+// at require  https://greasyfork.org/scripts/24851-wazewrap/code/WazeWrap.js
+
+var VERSION = '0.0.2';
+var shortcutEmptyStreet = "u"; // to move to a config panel, once...
 var selectedItems;
 var UpdateObject,
     AddOrGetCity,
@@ -61,7 +64,7 @@ function WMEEmptyStreet_bootstrap() {
  */
     // own code here
 
-    log("Start initialisation for:"+WazeWrap.User.Username());
+    //    log("Start initialisation for:"+WazeWrap.User.Username());
     //not sure how require works but we need the Action
     if (typeof(require) !== "undefined") {
         UpdateObject = require("Waze/Action/UpdateObject");
@@ -91,10 +94,10 @@ function WMEEmptyStreet_init() {
         return;
     }
 
+
     function countNewSegments() {
         var segments = W.selectionManager.selectedItems;
         var segmentCount = 0;
-        log("segmentLength" + segments.length);
         if (segments.length === 0 || segments[0].model.type !== 'segment') {
             log("No segments selected");
             return 0;
@@ -105,95 +108,16 @@ function WMEEmptyStreet_init() {
                 segmentCount += 1;
             }
         });
-        log("Nr of new segments" + segmentCount);
+        //        log("Nr of new segments" + segmentCount);
         return segmentCount;
     }
-
-    // Code below from MapOMatic source: WME ClickSaver 0.8.2 script
-    function getConnectedSegmentIDs(segmentID) {
-        var IDs = [];
-        var segment = W.model.segments.get(segmentID);
-        [W.model.nodes.get(segment.attributes.fromNodeID), W.model.nodes.get(segment.attributes.toNodeID)].forEach(function(node) {
-            if (node) {
-                node.attributes.segIDs.forEach(function(segID) {
-                    if (segID !== segmentID) {
-                        IDs.push(segID);
-                    }
-                });
-            }
-        });
-        return IDs;
-    }
-
-    function getFirstConnectedStateID(startSegment) {
-        var stateID = null;
-        var nonMatches = [];
-        var segmentIDsToSearch = [startSegment.attributes.id];
-        while (stateID === null && segmentIDsToSearch.length > 0) {
-            var startSegmentID = segmentIDsToSearch.pop();
-            startSegment = W.model.segments.get(startSegmentID);
-            var connectedSegmentIDs = getConnectedSegmentIDs(startSegmentID);
-            for (var i = 0; i < connectedSegmentIDs.length; i++) {
-                var streetID = W.model.segments.get(connectedSegmentIDs[i]).attributes.primaryStreetID;
-                if (streetID !== null && typeof(streetID) !== 'undefined') {
-                    var cityID = W.model.streets.get(streetID).cityID;
-                    stateID = W.model.cities.get(cityID).attributes.stateID;
-                    break;
-                }
-            }
-
-            if (stateID === null) {
-                nonMatches.push(startSegmentID);
-                connectedSegmentIDs.forEach(function(segmentID) {
-                    if (nonMatches.indexOf(segmentID) === -1 && segmentIDsToSearch.indexOf(segmentID) === -1) {
-                        segmentIDsToSearch.push(segmentID);
-                    }
-                });
-            } else {
-                return stateID;
-            }
-        }
-        return null;
-    }
-
-    function getFirstConnectedCityID(startSegment) {
-        var cityID = null;
-        var nonMatches = [];
-        var segmentIDsToSearch = [startSegment.attributes.id];
-        while (cityID === null && segmentIDsToSearch.length > 0) {
-            var startSegmentID = segmentIDsToSearch.pop();
-            startSegment = W.model.segments.get(startSegmentID);
-            var connectedSegmentIDs = getConnectedSegmentIDs(startSegmentID);
-            for (var i = 0; i < connectedSegmentIDs.length; i++) {
-                var streetID = W.model.segments.get(connectedSegmentIDs[i]).attributes.primaryStreetID;
-                if (streetID !== null && typeof(streetID) !== 'undefined') {
-                    cityID = W.model.streets.get(streetID).cityID;
-                    break;
-                }
-            }
-
-            if (cityID === null) {
-                nonMatches.push(startSegmentID);
-                connectedSegmentIDs.forEach(function(segmentID) {
-                    if (nonMatches.indexOf(segmentID) === -1 && segmentIDsToSearch.indexOf(segmentID) === -1) {
-                        segmentIDsToSearch.push(segmentID);
-                    }
-                });
-            } else {
-                return cityID;
-            }
-        }
-        return null;
-    }
-
-    // Code above from MapOMatic source: WME ClickSaver 0.8.2 script
 
 
     function setEmptyStreetAndCity() {
         // Most code from WME ClickSaver 0.8.2 script from MapOMatic
         var segmentCount = 0;
         var segments = W.selectionManager.selectedItems;
-//        log("segmentLength" + segments.length);
+        //        log("segmentLength" + segments.length);
 
         if (segments.length === 0 || segments[0].model.type !== 'segment') {
             alert("emptyStreetAndCity should not have been invoked");
@@ -208,28 +132,22 @@ function WMEEmptyStreet_init() {
             var segModel = segment.model;
             // this script is intended only to process not yet confirmed streets
             if (segModel.attributes.primaryStreetID === null) {
-                log('Selection type:' + segModel.type);
-                log('Updating segment:' + segModel.attributes.id);
-                var emptyCityID =  W.model.streets.getByAttributes({isEmpty:true})[0].cityID;
-                log('cityID found:' + emptyCityID);
+                var emptyCityID = W.model.streets.getByAttributes({
+                    isEmpty: true
+                })[0].cityID;
                 var emptyState = W.model.cities.objects[emptyCityID];
-                log('stateID found:' + emptyState.attributes.stateID);
                 var country = W.model.countries.get(W.model.countries.top.id);
-                log('In country:' + country.abbr);
-
                 var addCityAction = new AddOrGetCity(emptyState, country, "");
                 var newStreet = {
                     isEmpty: true,
                     cityID: emptyCityID
                 };
                 var emptyStreet = W.model.streets.getByAttributes(newStreet)[0];
-                log("emptyStreet ID:"+emptyStreet.id);
                 var emptyCity = Waze.model.cities.objects[emptyCityID];
                 var addStreetAction = new AddOrGetStreet("", emptyCity, true);
                 var action3 = new UpdateObject(segModel, {
                     primaryStreetID: emptyStreet.id
                 });
-                log("addStreetAction:"+ addStreetAction);
                 //construct the change
                 var m_action = new MultiAction();
                 m_action.setModel(W.model);
@@ -240,26 +158,21 @@ function WMEEmptyStreet_init() {
             }
             segmentCount += 1;
         });
-        log("Amount of segments set to empty:" + segmentCount);
+//        log("Amount of segments set to empty:" + segmentCount);
     }
 
 
     WMEEmptyStreet.makeButton = function(receiver) {
-        var toProcess = countNewSegments();
-        if (toProcess == 1) {
-            var _button = document.createElement("button");
-            _button.id = "emptyStreetButton";
-            _button.className = "btn btn-default";
-            _button.style = "float: right; height: 20px;line-height: 20px;padding-left: 8px;padding-right: 8px;margin-right: 4px;padding-top: 1px; margin-top:3px";
-            _button.title = "Check the emptyStreet and emptyCity checkboxes";
-            _button.innerHTML = "E";
-            _button.onclick = function() {
-                setEmptyStreetAndCity();
-            };
-            receiver.append(_button);
-        } else {
-  //          log("not showing a button . Amount of segments counted" + toProcess);
-        }
+        var _button = document.createElement("button");
+        _button.id = "emptyStreetButton";
+        _button.className = "btn btn-default";
+        _button.style = "float: right; height: 20px;line-height: 20px;padding-left: 4px;padding-right: 4px;margin-right: 4px;padding-top: 2px; margin-top:2px";
+        _button.title = "Check the emptyStreet and emptyCity checkboxes";
+        _button.innerHTML = shortcutEmptyStreet;
+        _button.onclick = function() {
+            setEmptyStreetAndCity();
+        };
+        receiver.append(_button);
     };
 
     // check for changes in the edit-panel
@@ -273,9 +186,14 @@ function WMEEmptyStreet_init() {
                 // Only fire up if it's a node
                 if (addedNode.nodeType === Node.ELEMENT_NODE) {
                     var emptyStreetDiv = addedNode.querySelector('div.clearfix.preview');
-
                     if (emptyStreetDiv) {
-                        WMEEmptyStreet.makeButton(emptyStreetDiv);
+                        // intent is to process 1 street at a time for multi selections please use other scripts or upgrade this one.
+                        var newSegmentCount = countNewSegments();
+                        var selectionlength = W.selectionManager.selectedItems.length;
+  //                      log("newSegmentCount " + newSegmentCount + " of " + selectionlength + " selections");
+                        if (newSegmentCount == 1 && selectionlength == 1) {
+                            WMEEmptyStreet.makeButton(emptyStreetDiv);
+                        }
                     }
 
                 }
@@ -283,6 +201,14 @@ function WMEEmptyStreet_init() {
         });
     });
 
+    // define new shortcut to call the emptyStreet routines
+    // currenty to run after street creation
+    if (I18n.locale == 'en') {
+        WMEKSRegisterKeyboardShortcut('WMEEmptyStreet', 'WME emptyStreet', 'Empty street segment', 'Set street and city to empty', setEmptyStreetAndCity, shortcutEmptyStreet);
+    } else {
+      log("Shortcut only active in english WME");
+    }
+    // A button for the edit panel as well
     emptyStreetObserver.observe(document.getElementById('edit-panel'), {
         childList: true,
         subtree: true
